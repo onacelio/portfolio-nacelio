7f1f30431a5441f04ff02fd8d017687d9d357995
O Nacélio! <nd7058898@gmail.com>
2023-08-15 13:32:11 -0300

    new project add

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,13 +1,15 @@
-import Project1 from "../../assets/project1.jpg"
-import Project2 from "../../assets/project2.jpg"
-import Project3 from "../../assets/project3.jpg"
-import Project4 from "../../assets/project4.png"
-import Project5 from "../../assets/project5.jpg"
-import Project6 from "../../assets/project6.jpg"
-import Project7 from "../../assets/project7.jpg"
-import Project8 from "../../assets/project8.jpg"
-import Project9 from "../../assets/project9.jpg"
-import Project10 from "../../assets/project10.jpg"
+import Project1 from "../../assets/projects/project1.jpg"
+import Project2 from "../../assets/projects/project2.jpg"
+import Project3 from "../../assets/projects/project3.jpg"
+import Project4 from "../../assets/projects/project4.png"
+import Project5 from "../../assets/projects/project5.jpg"
+import Project6 from "../../assets/projects/project6.jpg"
+import Project7 from "../../assets/projects/project7.jpg"
+import Project8 from "../../assets/projects/project8.jpg"
+import Project9 from "../../assets/projects/project9.jpg"
+import Project10 from "../../assets/projects/project10.jpg"
+import Project11 from "../../assets/projects/project11.png"
+import Project12 from "../../assets/projects/project12.png"
 
 export const projectsData = [
     {
@@ -79,7 +81,21 @@ export const projectsData = [
         title: "Discord Informática",
         category: "front",
         link: "https://github.com/onacelio/discordInformatica"
-    }
+    },
+     {
+        id: 11,
+        image: Project11,
+        title: "Trabalho de quimíca",
+        category: "front",
+        link: "https://github.com/onacelio/work-chemical"
+     },
+     {
+        id: 12,
+        image: Project12,
+        title: "API de moléculas",
+        category: "back",
+        link: "https://github.com/onacelio/work-chemical-backend"
+     }
 ]
 
 export const projectsNav = [
